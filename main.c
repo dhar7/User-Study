f74cf5fd648931543adf49c6a59e9b2f3c3b2311
Dharattacker <dharoy7939@gmail.com>
2024-10-25 11:53:11 +0600

    Multiplication by assembly language

--- main.c
+++ main.c
@@ -1,24 +1,15 @@
 #include <stdio.h>
 
-// Function to multiply two 2x2 matrices in pure C
-void matmul_c(int A[2][2], int B[2][2], int C[2][2]) {
-    for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < 2; j++) {
-            C[i][j] = 0;
-            for (int k = 0; k < 2; k++) {
-                C[i][j] += A[i][k] * B[k][j];
-            }
-        }
-    }
-}
+// Declaration of the assembly function
+extern void matmul_asm(int A[2][2], int B[2][2], int C[2][2]);
 
 int main() {
     int A[2][2] = { {1, 2}, {3, 4} };
     int B[2][2] = { {5, 6}, {7, 8} };
     int C[2][2];  // Resultant matrix
 
-    // Call matrix multiplication in C
-    matmul_c(A, B, C);
+    // Call matrix multiplication using assembly
+    matmul_asm(A, B, C);
 
     // Print the result
     printf("Matrix C (Result):\n");
